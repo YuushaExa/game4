e9bd32d9395986db5304e3660c997c974c532560
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-07-01 01:13:32 +0300

    Update game.js

--- game.js
+++ game.js
@@ -47,7 +47,7 @@ const heroes = {
 };
 
 const gameState = {
-    globalCount: 10,
+    globalCount: 0,
     previousScene: null,
     selectedHero: null,  
     party: []
@@ -102,7 +102,7 @@ const gameData = {
                         <button class="start-btn" next_scene="party">Manage Party</button>
                                                 <button class="start-btn" next_scene="stage1">stage 1</button>
          <button class="start-btn" next_scene="block_14">stage 14</button>
-             ${gameState.globalCount === 11 ? '<button class="start-btn" next_scene="block_map">MAP</button>' : ''}
+             ${gameState.globalCount === 10 ? '<button class="start-btn" next_scene="block_map">MAP</button>' : 'not enough points'}
                         </div>
             `,
     buttons: [
